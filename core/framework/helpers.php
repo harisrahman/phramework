3b9e8e9c1a56fb5e75227542abe895892d63687e
Haris Rahman <iharisrahman@yahoo.com>
2019-08-26 23:23:02 +0530

    regex route format to be changed

--- core/framework/helpers.php
+++ core/framework/helpers.php
@@ -7,6 +7,7 @@ class Helpers
 	public function request()
 	{
 		$parser = new Core\Routing\UrlParser();
+		var_dump($_SERVER['REQUEST_URI']);
 		$this->uri = $parser->parse_url($_SERVER['REQUEST_URI']);
 		$this->query = $parser->parse_query_string($_SERVER["QUERY_STRING"]);
 
