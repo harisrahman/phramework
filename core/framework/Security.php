e4ea15af2122f3a0017b2468f6bfe4e63b0ae161
Haris Rahman <iharisrahman@yahoo.com>
2019-10-06 23:58:39 +0530

    Support for old php versions in security class

--- core/framework/Security.php
+++ core/framework/Security.php
@@ -4,24 +4,37 @@ namespace Core\Framework;
 
 class Security
 {
-	public function generate_csrf_token()
+	public function generate_csrf_token() : string
 	{
 		$token = $_SESSION['csrf_token'] ?? null;
 
 		if (empty($token))
 		{
-			$_SESSION['csrf_token'] = $token = bin2hex(random_bytes(32));
+			if (function_exists('random_bytes'))
+			{
+				$token = bin2hex(random_bytes(32));
+			}
+			elseif (function_exists('mcrypt_create_iv'))
+			{
+				$token = bin2hex(mcrypt_create_iv(32, MCRYPT_DEV_URANDOM));
+			}
+			else
+			{
+				$token = bin2hex(openssl_random_pseudo_bytes(32));
+			}
+
+			$_SESSION['csrf_token'] = $token;
 		}
 
 		return $token;
 	}
 
-	public function verify_csrf_token()
+	public function verify_csrf_token() : bool
 	{
 		return !empty($_POST['csrf_token']) && hash_equals($_SESSION['csrf_token'], $_POST['csrf_token']);
 	}
 
-	public function csrf(bool $only_token = false)
+	public function csrf(bool $only_token = false) : string
 	{
 		$token = $this->generate_csrf_token();
 
@@ -35,7 +48,7 @@ class Security
 		}
 	}
 
-	public function xss(string $text)
+	public function xss(string $text) : string
 	{
 		return htmlspecialchars($text);
 	}
